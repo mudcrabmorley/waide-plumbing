1640fb3b364f6ced2ff6d8b6ae2fe8c5542f33da
Jake Morley <jakemorley@Jakes-MacBook-Pro.local>
2024-02-04 11:25:07 +1000

    services images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
 
       fontFamily: {
-        montserrat: ["Plus Jakarta Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
     },
   },
