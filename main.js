78e47ed8df7169d3d92388647e660a15dd856c5a
Jake Morley <jakemorley@Jakes-MacBook-Pro.local>
2024-02-14 17:48:38 +1000

    pages created

---
+++ main.js
@@ -0,0 +1,10 @@
+// HEADER/FOOTER FETCH
+window.onload = function () {
+  fetch(`./header.html`)
+    .then((response) => response.text())
+    .then((data) => (document.querySelector(`#header`).innerHTML = data));
+
+  fetch("./footer.html")
+    .then((response) => response.text())
+    .then((data) => (document.querySelector(`#footer`).innerHTML = data));
+};
